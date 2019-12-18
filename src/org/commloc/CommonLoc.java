4e9a58e0664c48044b0cd064cd07535cb141d761
sharms24 <sharmijaya123@gmail.com>
2019-12-18 11:56:41 +0530

    new txn

--- src/org/commloc/CommonLoc.java
+++ src/org/commloc/CommonLoc.java
@@ -1,11 +1,15 @@
 package org.commloc;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class CommonLoc {
 
@@ -14,9 +18,14 @@ public class CommonLoc {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.amazon.in/");
 		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(200,TimeUnit.SECONDS);
+		WebDriverWait w=new WebDriverWait(driver, 20);
+		
 		
 		driver.findElement(By.id("twotabsearchtextbox")).sendKeys("Redmi note 7 pro");
 		driver.findElement(By.xpath("//input[@value='Go']")).click();
+		
+		
 		
 		List<WebElement> red = driver.findElements(By.xpath("//span[@class='a-size-medium a-color-base a-text-normal']"));
 		
@@ -32,18 +41,7 @@ public class CommonLoc {
 			
 		}
 		
-		System.out.println("Print");
 		
-		
-		
-		System.out.println("Hai");
-		System.out.println("Hello");
-		System.out.println("sharmi");
-		System.out.println("Hell");
-		
-		WebElement buy = driver.findElement((By.xpath("(//a[@class='nav-a  '])[1])")));
-		buy.click();
-		System.out.println("Donefully");
 		
 		
 	}
